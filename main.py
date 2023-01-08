2fe38bc8a4ae52d3b005326458f789f0187c955a
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2023-01-08 19:43:08 +0200

    modified main.py

--- main.py
+++ main.py
@@ -32,9 +32,7 @@ st.set_page_config (page_title = 'Fincial Calculator:  Monthly Installment Estim
 # modify app display
 hide_menu = """
 <style>
-#MainMenu {
-    visibility: hidden;
-    }
+
 
 footer {
     visibility: visible;
